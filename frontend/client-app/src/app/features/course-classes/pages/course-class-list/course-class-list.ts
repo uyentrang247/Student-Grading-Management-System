6f7bc5274fa9e39a5aa790e19a2cb6701295cf93
Luong Bao Yen <yenluongbao9@gmail.com>
2026-05-28 23:04:00 +0700

    add course class search

--- frontend/client-app/src/app/features/course-classes/pages/course-class-list/course-class-list.ts
+++ frontend/client-app/src/app/features/course-classes/pages/course-class-list/course-class-list.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { CourseClass } from '../../models/course-class.model';
 import { CourseClassService } from '../../services/course-class.service';
 
+
 @Component({
   selector: 'app-course-class-list',
 
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
 
   templateUrl: './course-class-list.html',
   styleUrls: ['./course-class-list.css']
@@ -17,14 +19,22 @@ export class CourseClassList implements OnInit {
 
   courseClasses: CourseClass[] = [];
 
+allCourseClasses: CourseClass[] = [];
+
+searchKeyword = '';
+
   constructor(
     private courseClassService: CourseClassService
   ) {}
 
-  ngOnInit(): void {
-    this.courseClasses =
-      this.courseClassService.getCourseClasses();
-  }
+ ngOnInit(): void {
+
+  this.allCourseClasses =
+    this.courseClassService.getCourseClasses();
+
+  this.courseClasses =
+    this.allCourseClasses;
+}
 
   deleteCourseClass(id: number) {
 
@@ -40,4 +50,41 @@ export class CourseClassList implements OnInit {
     this.courseClasses =
       this.courseClassService.getCourseClasses();
   }
+  filterCourseClasses(): void {
+
+  const keyword =
+    this.searchKeyword.toLowerCase();
+
+  this.courseClasses =
+    this.allCourseClasses.filter(courseClass =>
+
+      courseClass.classCode
+        .toLowerCase()
+        .includes(keyword)
+
+      ||
+
+      courseClass.subjectName
+        .toLowerCase()
+        .includes(keyword)
+
+      ||
+
+      courseClass.lecturerName
+        .toLowerCase()
+        .includes(keyword)
+
+      ||
+
+      courseClass.semester
+        .toLowerCase()
+        .includes(keyword)
+
+      ||
+
+      courseClass.academicYear
+        .toLowerCase()
+        .includes(keyword)
+    );
+}
 }
