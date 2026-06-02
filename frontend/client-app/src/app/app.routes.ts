53fc04f8c21ad1c83e7cf3bec639f32fea7b7edd
Nguyen Thu Thao <thuthao6424@gmail.com>
2026-06-02 20:43:39 +0700

    feat: frontend - implement forgot password UI, route and service integration

--- frontend/client-app/src/app/app.routes.ts
+++ frontend/client-app/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-
 import { LoginComponent } from './features/auth/login/login';
 import { HomeComponent } from './features/home/home';
 import { StudentListComponent } from './features/student-management/student-list/student-list';
@@ -8,52 +7,55 @@ import { CourseClassForm } from './features/course-classes/course-class-form/cou
 import { SubjectList } from './features/subjects/subject-list/subject-list';
 import { SubjectForm } from './features/subjects/subject-form/subject-form';
 import { GradeEntryComponent } from './features/grade-management/grade-entry/grade-entry';
-
+import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password';
 export const routes: Routes = [
-  
   {
     path: '',
-    component: HomeComponent
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
-
   {
     path: 'login',
     component: LoginComponent
   },
-
+  {
+    path: 'home',
+    component: HomeComponent
+  },
   {
     path: 'student',
     component: StudentListComponent
   },
-  
   {
-  path: 'course-classes',
-  component: CourseClassList
-},
-{
-  path: 'course-classes/create',
-  component: CourseClassForm
-},
-{
-  path: 'course-classes/edit/:id',
-  component: CourseClassForm
-},
-{
+    path: 'course-classes',
+    component: CourseClassList
+  },
+  {
+    path: 'course-classes/create',
+    component: CourseClassForm
+  },
+  {
+    path: 'course-classes/edit/:id',
+    component: CourseClassForm
+  },
+  {
     path: 'subjects',
     component: SubjectList
   },
-
   {
     path: 'subjects/create',
     component: SubjectForm
   },
-
   {
     path: 'subjects/edit/:id',
     component: SubjectForm
   },
   {
-  path: 'grades',
-  component: GradeEntryComponent
-}
+    path: 'grades',
+    component: GradeEntryComponent
+  },
+  { 
+    path: 'forgot-password', 
+    component: ForgotPasswordComponent 
+  }
 ];
