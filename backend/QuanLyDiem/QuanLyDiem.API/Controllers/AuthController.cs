e986f45c6522e5fa950c76d5ca5a1f948018aa09
Nguyen Thu Thao <thuthao6424@gmail.com>
2026-06-03 10:20:04 +0700

    Fix merge conflict in AuthController

--- backend/QuanLyDiem/QuanLyDiem.API/Controllers/AuthController.cs
+++ backend/QuanLyDiem/QuanLyDiem.API/Controllers/AuthController.cs
@@ -28,12 +28,14 @@ namespace QuanLyDiem.API.Controllers
             return Ok(result);
         }
 
-       [HttpPost("forgot-password")]
-    public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDto model)
-    {
-        var result = await _authService.ForgotPasswordAsync(model);
-        if (result == null) return NotFound(new { message = "Email không tồn tại!" });
-        return Ok(new { message = "Thành công", data = result });
-    }
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDto model)
+        {
+            var result = await _authService.ForgotPasswordAsync(model);
+            
+            if (result == null) return NotFound(new { message = "Email không tồn tại!" });
+            
+            return Ok(new { message = "Thành công", data = result });
+        }
     }
 }
