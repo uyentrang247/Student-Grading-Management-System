aa35e1af038d16c6e472ea054e706d0fed726e27
eirian10 <nhujuank1009@email.com>
2026-06-16 23:10:15 +0700

    delete authorize

--- backend/QuanLyDiem/QuanLyDiem.API/Controllers/SubjectsController.cs
+++ backend/QuanLyDiem/QuanLyDiem.API/Controllers/SubjectsController.cs
@@ -2,11 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using QuanLyDiem.API.Data;
 using QuanLyDiem.API.Models;
-using Microsoft.AspNetCore.Authorization;
 
 namespace QuanLyDiem.API.Controllers
 {
-    [Authorize(Roles = "Admin")] 
     [Route("api/[controller]")]
     [ApiController]
     public class SubjectsController : ControllerBase
